9db064f4ec34972428db93fe9928e7c171239cdc
vatsal-prajapati-1 <prajapativatsal555@gmail.com>
2025-05-21 22:48:41 +0530

    first commit

---
+++ src/components/Header/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="top-strip py-2 border-t-[1px] border-gray-250 border-b-[1px]">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div className="col1-[50%]">
+            <p className="text-[12px] font-[500]">
+              Get up to 50% off new season style, limited time only
+            </p>
+          </div>
+
+          <div className="col2 flex items-center justify-end">
+            <ul className="flex items-center gap-3">
+              <li className="list-none">
+                <Link
+                  href="/help-center"
+                  className="text-[13px] link font-[500] transition"
+                >
+                  Help Center{" "}
+                </Link>
+              </li>
+              <li className="list-none">
+                <Link
+                  href="/order-tracking"
+                  className="text-[13px] link font-[500] transition"
+                >
+                  Order Tracking
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
